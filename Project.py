f1e2247994f9da6ada8e2433b3ad4e09e6005930
Rightful <alirazag5g6@gmail.com>
2022-08-28 05:41:08 -0700

    Just refactored the code and added total marks

--- Project.py
+++ Project.py
@@ -1,18 +1,15 @@
 student_dict = {}
 
 def dict_add(student_name,student_list):
-    student_dict[student_name] = student_list
-    total_marks = student_dict.get(student_name)
     n = 0
-    for x in total_marks:
+    for x in student_list:
         n += x
     student_dict[student_name] = n
-    return n
 
 def marks():
-    physics = int(input("Enter marks attained in physics: \n"))
-    chemistry = int(input("Enter marks attained in chemistry: \n"))
-    math = int(input("Enter marks attained in maths: \n"))
+    physics = float(input("Enter marks attained in physics: \n"))
+    chemistry = float(input("Enter marks attained in chemistry: \n"))
+    math = float(input("Enter marks attained in maths: \n"))
     return [physics,chemistry,math]
 
 def get_position(stud1,stud2,stud3,stud4,stud5):
@@ -26,26 +23,26 @@ def get_position(stud1,stud2,stud3,stud4,stud5):
 
 print("Current student: Zeeshan, Roll no: 0")
 zeeshan_subject_marks = marks()
-zeeshan_total_marks = dict_add("Zeeshan",zeeshan_subject_marks)
+dict_add("Zeeshan",zeeshan_subject_marks)
 
 print("Current student: Umair, Roll no: 1")
 umair_subject_marks = marks()
-umair_total_marks = dict_add("Umair",umair_subject_marks)
+dict_add("Umair",umair_subject_marks)
 
 print("Current student: Imran, Roll no: 2")
 imran_subject_marks = marks()
-imran_total_marks = dict_add("Imran",imran_subject_marks)
+dict_add("Imran",imran_subject_marks)
 
 print("Current student: Waleed, Roll no: 3")
 waleed_subject_marks = marks()
-waleed_total_marks = dict_add("Waleed",waleed_subject_marks)
+dict_add("Waleed",waleed_subject_marks)
 
 print("Current student: Arsalan, Roll no: 4")
 arsalan_subject_marks = marks()
-arsalan_total_marks = dict_add("Arsalan",arsalan_subject_marks)
+dict_add("Arsalan",arsalan_subject_marks)
 
-position = get_position(zeeshan_total_marks,umair_total_marks,imran_total_marks,waleed_total_marks,arsalan_total_marks)
+position = get_position(student_dict["Zeeshan"],student_dict["Umair"],student_dict["Imran"],student_dict["Waleed"],student_dict["Arsalan"])
 student_list = list(student_dict)
-print(f'''First Position:{position[0]} Roll no:{student_list.index(position[0])}
-Second Position:{position[1]} Roll no:{student_list.index(position[1])}
-Third Position:{position[2]} Roll no: {student_list.index(position[2])}''')
+print(f'''First Position:{position[0]} Roll no:{student_list.index(position[0])} who achieved {student_dict.get(position[0])} marks out of 300
+Second Position:{position[1]} Roll no:{student_list.index(position[1])} who achieved {student_dict.get(position[1])} marks out of 300
+Third Position:{position[2]} Roll no:{student_list.index(position[2])} who achieved {student_dict.get(position[2])} marks out of 300''')
